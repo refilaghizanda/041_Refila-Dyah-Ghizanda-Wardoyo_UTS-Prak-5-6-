ff2a7e47ff4bc134c9e53fa99ee1ca0b1df32fc2
Refila Dyah Ghizanda Wardoyo <90167525+refilaghizanda@users.noreply.github.com>
2022-10-19 20:35:34 +0700

    Add files via upload

---
+++ UTS/Praktikum 5/041_Praktikum 5 Nomor 2.php
@@ -0,0 +1,69 @@
+<!-- 21091397041 Refila Dyah Ghizanda Wardoyo -->
+
+<?php
+class Pegawai
+{
+    public $name;
+    public $gaji;
+    public function __construct($name, $gaji)
+    {
+        $this->name = $name;
+        $this->gaji = $gaji;
+    }
+    public function infoGaji()
+    {
+        return $this->gaji;
+    }
+}
+class Manager extends Pegawai
+{
+    private $tunjangan;
+    public function __construct($name, $gaji, $tunjangan)
+    {
+        parent::__construct($name, $gaji);
+        $this->tunjangan = $tunjangan;
+    }
+    public function infoGaji()
+    {
+        return $this->gaji;
+    }
+    public function infoTunjangan()
+    {
+        return $this->tunjangan;
+    }
+}
+class Programmer extends Pegawai
+{
+    private $bonus;
+    public function __construct($name, $gaji, $bonus)
+    {
+        parent::__construct($name, $gaji);
+        $this->bonus = $bonus;
+    }
+    public function infoBonus()
+    {
+        return $this->bonus;
+    }
+}
+class Bayaran
+{
+    public function hitungBayaran($peg)
+    {
+        $uang = $peg->infoGaji();
+
+        return $uang;
+    }
+    public static function main($args)
+    {
+        echo "Refila Dyah Ghizanda Wardoyo", "\n";
+        echo "21091397041", "\n";
+        echo "<br>", "<br>";
+        $man = new Manager("Refila", 20000000, 45);
+        $prog = new Programmer("Dyah", 18000000, 30);
+        $hr = new Bayaran();
+        echo "<br>Gaji Manager ". $man->name." : RP. ".strval($hr->hitungBayaran($man)), "\n";
+        echo "<br>Gaji Programmer ". $prog->name." : RP. ".strval($hr->hitungBayaran($prog)), "\n";
+    }
+}
+Bayaran::main(array());
+?>
---
+++ UTS/Praktikum 6/No4/No4interface.php
@@ -0,0 +1,14 @@
+<!-- Refila Dyah Ghizanda Wardoyo (21091397041) -->
+
+<?php 
+
+interface Flyer {
+    public function takeOff();
+    public function land();
+    public function fly();
+}
+
+interface Sailer {
+    public function dock();
+    public function cruise();
+}
